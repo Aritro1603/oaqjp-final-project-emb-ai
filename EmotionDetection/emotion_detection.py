17410c3fd7f989ed1059bf1b2db13b6012bd5b38
Aritro1603 <aritroghosal2003@gmail.com>
2026-09-10 08:03:08 -0400

    Add error handling for blank input

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,6 +3,7 @@ import requests
 
 
 def emotion_detector(text_to_analyze):
+
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
 
     headers = {
@@ -22,31 +23,45 @@ def emotion_detector(text_to_analyze):
         timeout=30
     )
 
-    response_data = json.loads(response.text)
+    if response.status_code == 200:
+        response_data = json.loads(response.text)
 
-    emotions = response_data["emotionPredictions"][0]["emotion"]
+        emotions = response_data["emotionPredictions"][0]["emotion"]
 
-    anger_score = emotions["anger"]
-    disgust_score = emotions["disgust"]
-    fear_score = emotions["fear"]
-    joy_score = emotions["joy"]
-    sadness_score = emotions["sadness"]
+        anger_score = emotions["anger"]
+        disgust_score = emotions["disgust"]
+        fear_score = emotions["fear"]
+        joy_score = emotions["joy"]
+        sadness_score = emotions["sadness"]
 
-    emotion_scores = {
-        "anger": anger_score,
-        "disgust": disgust_score,
-        "fear": fear_score,
-        "joy": joy_score,
-        "sadness": sadness_score
-    }
+        emotion_scores = {
+            "anger": anger_score,
+            "disgust": disgust_score,
+            "fear": fear_score,
+            "joy": joy_score,
+            "sadness": sadness_score
+        }
 
-    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+        dominant_emotion = max(
+            emotion_scores,
+            key=emotion_scores.get
+        )
 
-    return {
-        "anger": anger_score,
-        "disgust": disgust_score,
-        "fear": fear_score,
-        "joy": joy_score,
-        "sadness": sadness_score,
-        "dominant_emotion": dominant_emotion
-    }
+        return {
+            "anger": anger_score,
+            "disgust": disgust_score,
+            "fear": fear_score,
+            "joy": joy_score,
+            "sadness": sadness_score,
+            "dominant_emotion": dominant_emotion
+        }
+
+    elif response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
